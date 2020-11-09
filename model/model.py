023f8821a1c5f7ec3871fa58e1ec79d7240bc4b4
hexmagic <191440042@qq.com>
2020-11-09 17:05:08 +0800

    add device asset

--- model/model.py
+++ model/model.py
@@ -1,12 +1,13 @@
 import segmentation_models_pytorch as spm
 
 class Model(object):
-    def __init__(self, model_name,class_num,encoder_weights='imagenet'):
+    def __init__(self, model_name,class_num,encoder_weights='imagenet',device=None):
         self.model_name = model_name
         self.class_num = class_num
+        self.device = device
         self.encoder_weights = encoder_weights
     
     def create_model(self):
         model =spm.Unet(self.model_name, encoder_weights=self.encoder_weights, classes=self.class_num,
-                                  activation=None)
+                                  activation=None).to(self.device)
         return model
