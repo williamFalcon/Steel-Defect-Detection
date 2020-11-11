43e929d5070d4b35c3852417a66ef9a5906449ca
William Falcon <waf2107@columbia.edu>
2020-11-11 16:44:44 -0500

    accelerator docs

--- train.py
+++ train.py
@@ -63,13 +63,19 @@ if __name__ == '__main__':
                   encoder=arg.encoder,
                   decoder=arg.decoder)
     train_loader, val_loader = create_dataloader(arg)
-    trainer = pl.Trainer(gpus=1,
+    import os
+    env = os.environ
+    print(env)
+    trainer = pl.Trainer(
                          log_gpu_memory=True,
                          benchmark=True,
                          accumulate_grad_batches=5,
                          auto_scale_batch_size='binsearch',
                          max_epochs=arg.epochs,
                          val_check_interval=0.5)
+    print('*' * 100)
+    print('GPUS:', trainer.gpus)
+    print('*' * 100)
     # log_gpu_memory=True, val_check_interval=0.5)
     #trainer.tune(model, train_loader, val_loader)
     trainer.fit(model,
